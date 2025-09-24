c157b1ad1618f5abdfa4f793d5b92c4d6fc811db
BohdanVovkotrub <bvovkotrub@gmail.com>
2025-09-24 12:09:58 +0300

    changed run.cmd

--- run.cmd
+++ run.cmd
@@ -8,11 +8,6 @@ call "./scripts/dotenv.cmd"
 call "./scripts/create-docker-network.cmd"
 call "./scripts/create-compose.cmd" "%DOCKER_COMPOSE_TEMPLATE_NAME%" "%DOCKER_COMPOSE_NAME%"
 
-exit /b %errorlevel%
-if %errorlevel% neq 0 (
-    echo Error occurred during setup. Exiting.
-    goto :eof
-)
 docker-compose -f "%DOCKER_COMPOSE_NAME%" up -d
 
 :eof
